964c17497a8d2dfeade6de9e7734d7999df7eb1d
Kristian Hansen <kmolha@gmail.com>
2018-04-23 00:54:43 +0200

    Working relative drone movement using quaternions and homogenous transforms

--- Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
+++ Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
@@ -1,11 +1,13 @@
 import rospy
-from python_node.srv import Yaw
+from python_node.srv import MoveDrone
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
 from mavros_msgs.srv import SetMode, CommandBool
 from genpy.message import fill_message_args
 from math import cos, sin, pi, atan2, asin, degrees
 from quaternion import *
+import numpy as np
+from numpy.linalg import inv
 
 current_state = State()
 current_pose = PoseStamped()
@@ -44,10 +46,11 @@ def main():
 	roll = 0 * pi / 180
 	pitch = 0 * pi / 180
 
-	w, x, y, z = euler_angle_2_quaternion(roll,pitch,yaw)
+	q = euler_angle_2_quaternion(roll,pitch,yaw)
+	print("Quaternion: {}".format(q))
 	global pose
 	pose = PoseStamped()
-	args = [{'pose': {'position': [0,0,2], 'orientation': [x,y,z,w]} }]
+	args = [{'pose': {'position': [0,0,2], 'orientation': [q.x, q.y, q.z, q.w]} }]
 	fill_message_args(pose, args)
 
 	rospy.loginfo(pose)
@@ -60,24 +63,51 @@ def main():
 	set_mode_client(custom_mode="OFFBOARD")
 	arming_client(value=True)
 
-	def handler(req):
+	def service_handler(req):
+		X =req.x
+		Y =req.y
+		Z =req.z
 		yaw = req.yaw * pi / 180
+
+		print(current_pose)
+
+		world2drone = posestamped_to_transform(current_pose)
+		transform = transformation_matrix(current_pose.pose.orientation,X,Y,Z,yaw)
+
+		# matrix_to_posestamped(transform)
+		# print(transform)
+
+		translation = np.array([[X],[Y],[Z],[1]])
+
+		new_pos = np.matmul(world2drone,translation)
+		# new_conf = np.matmul(world2drone,transform)
+		# global pose
+		# pose = matrix_to_posestamped(new_conf)
+		# # print(new_conf)
+
 		rospy.loginfo("Yawing {} degrees".format(yaw*180/pi))
-		w, x, y, z = euler_angle_2_quaternion(roll, pitch, yaw)
-		delta_pose = PoseStamped()
-		args = [{'pose': {'position': [0, 0, 2], 'orientation': [x, y, z, w]}}]
-		fill_message_args(delta_pose, args)
 
 		global pose
-		pose = PoseStamped()
+		q = perform_yaw(current_pose, yaw)
+		args = [{'pose': {'position': [new_pos[0],new_pos[1],new_pos[2]],
+						  'orientation': [q.x, q.y, q.z, q.w]} }]
+		fill_message_args(pose,args)
+		# w, x, y, z = euler_angle_2_quaternion(roll, pitch, yaw)
+		# delta_pose = PoseStamped()
+		# args = [{'pose': {'position': [0, 0, 2], 'orientation': [x, y, z, w]}}]
+		# fill_message_args(delta_pose, args)
+		#
+		# global pose
+		# pose = PoseStamped()
+		#
+		# w, x, y, z = quaternion_mult(current_pose.pose.orientation, delta_pose.pose.orientation)
+		# args = [{'pose': {'position': [new_pos[0], new_pos[1], new_pos[2]], 'orientation': [x, y, z, w]}}]
+		# fill_message_args(pose, args)
 
-		w, x, y, z = quaternion_mult(current_pose.pose.orientation, delta_pose.pose.orientation)
-		args = [{'pose': {'position': [0, 0, 2], 'orientation': [x, y, z, w]}}]
-		fill_message_args(pose, args)
 
 		return True
 
-	rospy.Service("turn_drone",Yaw,handler)
+	rospy.Service("move_drone",MoveDrone,service_handler)
 
 	while not rospy.is_shutdown():
 		pub.publish(pose)
