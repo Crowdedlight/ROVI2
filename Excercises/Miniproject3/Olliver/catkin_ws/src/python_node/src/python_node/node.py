8117350b235debf9ec9c2083512be9ac3db1f83f
Kristian Hansen <kmolha@gmail.com>
2018-04-22 15:03:57 +0200

    Working python offboard node

--- Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
+++ Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
@@ -1,14 +1,33 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
-import genpy
-import rosservice
-import roslib.packages
+from mavros_msgs.srv import SetMode, CommandBool
+
+global current_state
 
 def callback(data):
+	current_state = data
 	rospy.loginfo(data)
 
 def main():
 	rospy.init_node("python_node")
 	rospy.Subscriber("mavros/state", State, callback)
-	rospy.spin()
+
+	pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped,queue_size=10)
+
+	set_mode_client = rospy.ServiceProxy("mavros/set_mode",SetMode)
+	set_mode_client(custom_mode="OFFBOARD")
+
+	arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
+	arming_client(value=True)
+
+	rate = rospy.Rate(10)
+
+	pose = PoseStamped()
+	pose.pose.position.x = -3
+	pose.pose.position.y = 5
+	pose.pose.position.z = 3
+
+	while not rospy.is_shutdown():
+		pub.publish(pose)
+		rate.sleep()
