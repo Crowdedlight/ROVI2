cca1db8cdb3220660681c32d3090c3ef54ef020b
Kristian Hansen <kmolha@gmail.com>
2018-04-22 14:01:57 +0200

    Working mavros state listener

--- Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
+++ Excercises/Miniproject3/Olliver/catkin_ws/src/python_node/src/python_node/node.py
@@ -1,7 +1,14 @@
-from rospy import init_node
+import rospy
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import State
 import genpy
 import rosservice
 import roslib.packages
 
+def callback(data):
+	rospy.loginfo(data)
+
 def main():
-	init_node("node")
+	rospy.init_node("python_node")
+	rospy.Subscriber("mavros/state", State, callback)
+	rospy.spin()
